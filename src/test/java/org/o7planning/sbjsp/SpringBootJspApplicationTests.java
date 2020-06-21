a1fe53c4f3b2254b4f2e96fd0c1b366b4aa9632f
tiendat182 <tiendat.fet4@gmail.com>
2020-06-21 11:25:31 +0700

    Tuts SpringBootJSP

---
+++ src/test/java/org/o7planning/sbjsp/SpringBootJspApplicationTests.java
@@ -0,0 +1,13 @@
+package org.o7planning.sbjsp;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJspApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
